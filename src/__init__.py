ecc56711b44874a6bc6a89363583e3bc03dc7697
NgocMinhUET <dongocminh@vnu.edu.vn>
2025-11-25 15:08:46 +0700

    Fix: Comment out imports for unimplemented modules
    - Only import existing modules (gop_manager, roi_detector, vvc_encoder, motion_vector_extractor)
    - Comment out temporal_propagator, hierarchical_roi, qp_controller, performance_evaluator
    - Will uncomment when modules are implemented
    
    This fixes ModuleNotFoundError when running exp1_baseline.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,22 +6,26 @@ Main package initialization
 __version__ = "1.0.0"
 __author__ = "Bui Thanh Huong, Do Ngoc Minh, Hoang Van Xiem"
 
+# Import existing modules
 from .gop_manager import GOPManager
 from .roi_detector import ROIDetector
-from .temporal_propagator import TemporalPropagator
-from .hierarchical_roi import HierarchicalROI
-from .qp_controller import QPController
 from .vvc_encoder import VVCEncoder
 from .motion_vector_extractor import MotionVectorExtractor
-from .performance_evaluator import PerformanceEvaluator
+
+# TODO: Uncomment when modules are implemented
+# from .temporal_propagator import TemporalPropagator
+# from .hierarchical_roi import HierarchicalROI
+# from .qp_controller import QPController
+# from .performance_evaluator import PerformanceEvaluator
 
 __all__ = [
     "GOPManager",
     "ROIDetector",
-    "TemporalPropagator",
-    "HierarchicalROI",
-    "QPController",
     "VVCEncoder",
     "MotionVectorExtractor",
-    "PerformanceEvaluator",
+    # TODO: Add when implemented
+    # "TemporalPropagator",
+    # "HierarchicalROI",
+    # "QPController",
+    # "PerformanceEvaluator",
 ]
